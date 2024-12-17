52a8017bebab146b5bd2f1c344753c9570480c68
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2024-12-17 14:54:10 -0800

    Removed unused/unneeded stuff

--- premake5.lua
+++ premake5.lua
@@ -4,8 +4,8 @@ project "spdlog"
     cppdialect "C++17"
     staticruntime "off"
 
-    targetdir ("../../Build/bin/%{prj.name}")
-    objdir    ("../../Build/obj/%{prj.name}")
+    targetdir ("" .. ThirdPartyOutputDir .. "/bin/%{prj.name}")
+    objdir    ("" .. ThirdPartyOutputDir .. "/obj/%{prj.name}")
 
     defines
     {
@@ -18,28 +18,28 @@ project "spdlog"
         "src/**.cpp",
         "include/**.h",
         "include/**.cpp",
-        "example/**.h",        
+        "example/**.h",
         "example/**.cpp"
     }
     
-	includedirs
-	{
-		"include",
+    includedirs
+    {
+        "include",
         "bench"
     }
 
-	filter "system:windows"
-		systemversion "latest"
+    filter "system:windows"
+        systemversion "latest"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
 
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
         symbols "off"
