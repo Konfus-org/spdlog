ab47aed1dffcc7f95a2e1c62d064ce018251d8fc
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-04-08 17:29:44 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -34,13 +34,16 @@ project "spdlog"
 
     filter "configurations:Debug"
         runtime "Debug"
+        linkoptions { "/LTCG:incremental" }  -- Enable Link Time Code Generation and Incremental linking
         symbols "on"
+
+    filter "configurations:Optimized"
+        runtime "Release"
+        linkoptions { "/LTCG:incremental" }  -- Enable Link Time Code Generation and Incremental linking
+        optimize "on"
 
     filter "configurations:Release"
         runtime "Release"
         optimize "on"
-
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "on"
+        linkoptions { "/LTCG:incremental" }  -- Enable Link Time Code Generation and Incremental linking
         symbols "off"
