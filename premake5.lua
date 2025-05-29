dbb663b74f1d0d3553c59f33044080b860c7ec38
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-05-28 17:06:57 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -4,25 +4,10 @@ project "spdlog"
     cppdialect "C++17"
     staticruntime "Off"
 
-    flags
-    {
-        "MultiProcessorCompile"
-    }
-
-    if OutputIntermediateDir == nil or OutputTargetDir == nil then
-        targetdir ("Build/bin/%{prj.name}/")
-        objdir    ("Build/obj/%{prj.name}/")
-
-    else
-        targetdir ("../../" .. OutputTargetDir .. "")
-        objdir    ("../../" .. OutputIntermediateDir .. "")
-    end
-
     defines
     {
         "SPDLOG_COMPILED_LIB"
     }
-
     files
     {
         "src/**.h",
@@ -32,25 +17,8 @@ project "spdlog"
         "./**.md",
         "./**.lua",
     }
-
     includedirs
     {
         "include",
         "bench"
     }
-
-    filter "configurations:Debug"
-        runtime "Debug"
-        buildoptions { "/MDd" } 
-        symbols "On"
-
-    filter "configurations:Optimized"
-        runtime "Release"
-        buildoptions { "/MDd" } 
-        optimize "On"
-
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "On"
-        buildoptions { "/MD" } 
-        symbols "Off"
